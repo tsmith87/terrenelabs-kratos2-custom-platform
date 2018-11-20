bae45ae0252ee4cf59ad9ead0660f409b460b396
tsmith87 <tim@terrenelabs.com>
2018-11-20 09:40:06 -0500

    updated scripts

--- Kratos2Platform_Ubuntu/builder/platform-uploads/opt/elasticbeanstalk/hooks/appdeploy/post/00-post-deploy-script.sh
+++ Kratos2Platform_Ubuntu/builder/platform-uploads/opt/elasticbeanstalk/hooks/appdeploy/post/00-post-deploy-script.sh
@@ -2,17 +2,14 @@
 
 instanceHost=$( cat /etc/hostname )
 [[ $(tail -n 1 /etc/hosts) != 127.0.1.1* ]] && echo "127.0.1.1 $instanceHost"| sudo tee -a /etc/hosts
-sudo rm -rf /tmp/hydra2.conf
+sudo rm -rf /tmp/kratos2.conf
 sudo rm -rf /tmp/var.txt
-sed -n '/^environment=/q;p' /etc/supervisor/conf.d/hydra2.conf > /tmp/hydra2.conf
-echo "environment=" >> /tmp/hydra2.conf
+sed -n '/^environment=/q;p' /etc/supervisor/conf.d/kratos2.conf > /tmp/kratos2.conf
+echo "environment=" >> /tmp/kratos2.conf
 sudo /opt/elasticbeanstalk/bin/get-config environment --output yaml | sed -n '1!p' | sed -e 's/^\(.*\): /\1=/g' > /tmp/var.txt
 sed '$!s/$/,/' /tmp/var.txt
-cat /tmp/var.txt | while read LINE; do echo $'\t'$LINE >> /tmp/hydra2.conf; done;
-sudo cp -f /tmp/hydra2.conf /etc/supervisor/conf.d/hydra2.conf
-source /opt/python/env/tl/bin/activate
-pip install -r /var/app/current/requirements.txt
-deactivate
+cat /tmp/var.txt | while read LINE; do echo $'\t'$LINE >> /tmp/kratos2.conf; done;
+sudo cp -f /tmp/kratos2.conf /etc/supervisor/conf.d/kratos2.conf
 sudo supervisorctl reread
 sudo service supervisor stop
 sudo service supervisor start
