9eb910d65d7a23d12bae1c9594d25ceb2874c4a5
tsmith87 <tim@terrenelabs.com>
2018-11-21 15:24:55 -0500

    fixing script to load supervisor conf file

--- Kratos2Platform_Ubuntu/builder/platform-uploads/opt/elasticbeanstalk/hooks/configdeploy/post/00-restart-supervisor.sh
+++ Kratos2Platform_Ubuntu/builder/platform-uploads/opt/elasticbeanstalk/hooks/configdeploy/post/00-restart-supervisor.sh
@@ -5,7 +5,8 @@ sudo rm -rf /tmp/var.txt
 sed -n '/^environment=/q;p' /etc/supervisor/conf.d/kratos2.conf > /tmp/kratos2.conf
 echo "environment=" >> /tmp/kratos2.conf
 sudo /opt/elasticbeanstalk/bin/get-config environment --output yaml | sed -n '1!p' | sed -e 's/^\(.*\): /\1=/g' > /tmp/var.txt
-sed '$!s/$/,/' /tmp/var.txt
+sed '$!s/$/,/' /tmp/var.txt > /tmp/var_tmp.txt
+sudo mv /tmp/var_tmp.txt /tmp/var.txt
 cat /tmp/var.txt | while read LINE; do echo $'\t'$LINE >> /tmp/kratos2.conf; done;
 sudo cp -f /tmp/kratos2.conf /etc/supervisor/conf.d/kratos2.conf
 sudo supervisorctl reread
